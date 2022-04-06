0b80343bb12cd433a0a3dd0708c5875071740ac7
Al Conlan <oconellan@gmail.com>
2022-04-06 17:07:46 -0500

    added database and imported data into table

--- import-data.py
+++ import-data.py
@@ -1,5 +1,10 @@
 import pandas as pd
+from sqlalchemy import create_engine
 
-df = pd.read_excel (r'C:\Projects\inerg-api\assets\20210309_2020_1 - 4.xls')
-print ('Data: ' , df)
+df = pd.read_excel ('assets/20210309_2020_1 - 4.xls')
+print (df.head())
+
+engine = create_engine('sqlite:///ohio-quarterly.db')
+
+df.to_sql('OhioWells', engine)
 
